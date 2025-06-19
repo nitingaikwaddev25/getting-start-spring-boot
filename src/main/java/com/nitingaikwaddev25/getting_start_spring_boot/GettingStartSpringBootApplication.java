2ac5a06ca689a0b0b0705103e75eeeec7256ead3
nitingaikwad293 <nitingaikwad293@gmail.com>
2025-06-19 20:43:03 +0800

    Initial Commit

---
+++ src/main/java/com/nitingaikwaddev25/getting_start_spring_boot/GettingStartSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.nitingaikwaddev25.getting_start_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingStartSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nitingaikwaddev25/getting_start_spring_boot/GettingStartSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitingaikwaddev25.getting_start_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingStartSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
